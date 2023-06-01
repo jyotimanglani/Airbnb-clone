1dfd9868d4a4c58b012e50397635e622e35b719c
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-06-01 13:24:38 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ export default function Card(props) {
     return (
          <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.item.coverImg}`} className="card--image" />
+            <img src={`/src/images/${props.item.coverImg}`} className="card--image" />
             <div className = "card--stats">
                 <img src={Star} className="card--star"/>
                 <span className="stats">{props.item.stats.rating}</span>
